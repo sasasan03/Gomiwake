f1421682d203b45794e7c5e379130e9bb51d1ab7
sasasan03 <sako.hiro62@gmail.com>
2023-01-21 21:01:52 +0900

    Initial Commit

---
+++ Gomiwake/GomiwakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GomiwakeApp.swift
+//  Gomiwake
+//
+//  Created by sako0602 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GomiwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
